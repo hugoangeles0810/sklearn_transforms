2d1f4f9e068c28f047b4a0b290bd45748f6dd0c1
Hugo Ángeles <hugo.angeles.chavez@ibm.com>
2020-08-18 23:56:32 -0500

    added custom transformers

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +17,38 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class FormatNaNColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        inputer = SimpleImputer(
+            missing_values=np.nan,  # los valores que faltan son del tipo ``np.nan`` (Pandas estándar)
+            strategy='mean',  # la estrategia elegida es cambiar el valor faltante por una constante
+            #fill_value=0,  # la constante que se usará para completar los valores faltantes es un int64 = 0
+            verbose=0,
+            copy=True
+        )
+        data = X.copy()
+        dataFormatted =  pd.DataFrame(inputer.fit_transform(data[self.columns].values), columns=self.columns, index = data.index)
+        data[self.columns] = dataFormatted
+        return data
+
+class NormalizeColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        scaler = MinMaxScaler()
+        data = X.copy()
+        df_scaled =  pd.DataFrame(scaler.fit_transform(data[self.columns].values), columns=self.columns, index = data.index)
+        data[self.columns] = df_scaled
+        return data
